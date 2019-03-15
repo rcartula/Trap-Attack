5f36ddea9c48a6aa8b137a67174c8301aaa5af5a
TJYadisernia <tyadiser@student.fitchburgstate.edu>
2019-03-15 01:25:50 -0400

    3/15

---
+++ Group_Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GameController : MonoBehaviour {
+    public GameObject[] powerups;
+    public Vector3 spawnValues;
+    public int powerCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnPowers());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator SpawnPowers()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < powerCount; i++)
+            {
+                GameObject powerUp = powerups[Random.Range(0, powerups.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range(-spawnValues.z, spawnValues.z));
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(powerUp, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+}
